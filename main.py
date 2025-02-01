09b1baaae16ad6286812a77e6fee46dac7d95d43
MauroRaya <maurorayabr@gmail.com>
2025-02-01 14:27:08 -0300

    feat: resize frame

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import cv2
 import ctypes
 
-user32 = ctypes.windll.user32
-
-window_x = user32.GetSystemMetrics(0)
-window_y = user32.GetSystemMetrics(1)
-
-print(f'MONITOR WIDTH:  {window_x}')
-print(f'MONITOR HEIGHT: {window_y}')
-
-window_center_x = int(window_x / 2)
-window_center_y = int(window_y / 2)
-
-print(f'MONITOR CENTER WIDTH:  {window_center_x}')
-print(f'MONITOR CENTER HEIGHT: {window_center_y}')
-
+WINDOW_NAME = 'Cars tracking'
 cap = cv2.VideoCapture('videos/cars.mp4')
 
-# while True:
-#     frames_left, frame = cap.read()
 
-#     if not frames_left:
-#         break
+def get_monitor_proportions() -> tuple[int, int]:
+    user32 = ctypes.windll.user32
 
-#     cv2.imshow('This is the title of the frame', frame)
+    window_x = user32.GetSystemMetrics(0)
+    window_y = user32.GetSystemMetrics(1)
 
-#     key = cv2.waitKey(10)
+    return (window_x, window_y)
 
-#     ESC = 27
-#     if key == ESC:
-#         break
+
+while True:
+    frames_left, frame = cap.read()
+
+    if not frames_left:
+        break
+
+    window_x, window_y = get_monitor_proportions()
+    resized_frame      = cv2.resize(frame, (window_x, window_y))
+
+    cv2.imshow(WINDOW_NAME, resized_frame)
+    cv2.moveWindow(WINDOW_NAME, 0, 0)
+
+    key = cv2.waitKey(10)
+
+    ESC = 27
+    if key == ESC:
+        break
